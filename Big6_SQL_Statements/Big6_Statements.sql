54be10429acbdec0cbe61e9dc584a769302bf011
RashwanthJeyaprakash <rashwanthjeyaprakash@gmail.com>
2025-05-09 11:27:07 +0530

    Update Big6_Statements.sql

--- Big6_SQL_Statements/Big6_Statements.sql
+++ Big6_SQL_Statements/Big6_Statements.sql
@@ -8,10 +8,16 @@ SAMPLE QUERIES
 SELECT *
 FROM actors;
 
+/*Select the first 100 of films release after 2000. */
+SELECT *
+FROM film
+WHERE release_year > 2000
+LIMIT 100;
+
 /*Find all customers from Canada*/
 Select *
-    FROM customers 
-    WHERE country = 'Canada';
+FROM customers 
+WHERE country = 'Canada';
 
 /*Retrieve a list with all  actors whose first name is Chris and have Won an Emmy. */
 SELECT *
@@ -24,6 +30,20 @@ SELECT *
 FROM actors
 WHERE first_name IN ('John', 'Chris');
 
+/*Select all data from the “film” table, ordering it by “release_date” in descending order.*/
+SELECT *
+FROM film
+ORDER BY release_date DESC;
 
+/* Count the number of films released each year .*/
+SELECT release_year, COUNT(film_id) AS total
+FROM film
+GROUP BY release_year;
 
-### 
+/*Finding customers who have rented more than 10 movies, and show the number of rentals they made in DESC order .*/
+SELECT customer_id, COUNT(rental_id) AS total_rentals
+FROM rental
+GROUP BY customer_id
+HAVING COUNT(rental_id) > 10
+ORDER BY total_rentals DESC;
+
